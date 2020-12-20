707238703a9f60773ae65582f10c416ddc159608
dkim22 <ehddnr1186@gmail.com>
2020-12-21 01:14:32 +0900

    ramdomize production classnames

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,16 +4,26 @@
 
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  createGenerateClassName,
+  StylesProvider,
+} from "@material-ui/core/styles";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "co",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
